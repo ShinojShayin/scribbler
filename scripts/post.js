532911acb8a092efa5b6eabe378aa6213d4374c7
Shinoj <ShinojShayin@users.noreply.github.com>
2021-03-08 14:59:31 +0530

    post page javascript based functionalities added

---
+++ scripts/post.js
@@ -0,0 +1,55 @@
+document.getElementById('savePost').onclick = function(){
+
+}
+
+document.getElementById('editPost').onclick = function(){
+    var titleEle =  document.getElementById('title-text');
+    var postTextEle = document.getElementById('post-text');
+    titleEle.setAttribute('contentEditable', 'true');
+    titleEle.className = 'editable';
+    titleEle.focus();
+    postTextEle.setAttribute('contentEditable', 'true');
+    postTextEle.className = 'post-body editable';
+    this.style.display = "none";
+    document.getElementById('savePost').style.display = "block";
+}
+
+document.getElementById('savePost').onclick = function(){
+    this.style.display = "none";
+    document.getElementById('editPost').style.display = "block";
+    var titleEle =  document.getElementById('title-text');
+    var postTextEle = document.getElementById('post-text');
+    titleEle.removeAttribute('contentEditable');
+    titleEle.className = '';
+    postTextEle.removeAttribute('contentEditable');
+    postTextEle.className = 'post-body';
+}
+
+document.getElementById('likeBtn').onclick = function(){
+    var likeCount = this.getAttribute('like-count');
+    if(!likeCount)likeCount=1;
+    else{
+        likeCount = parseInt(likeCount) + 1;
+    }
+    this.setAttribute('like-count',likeCount);
+
+    if(likeCount==1){
+        document.getElementById('like-text').innerHTML = "Liked";
+        document.getElementById('liked-people').innerHTML = "1 person likes this!";
+    }
+    else{
+        document.getElementById('liked-people').innerHTML = likeCount + " people likes this!";
+    }
+}
+
+document.getElementById('submit-comment').onclick = function(){
+    var commentBox = document.getElementById('commentbox');
+    var comment = commentBox.value
+    if(comment.trim()!=""){
+        var comEle = document.createElement("div");
+        comEle.className = "user-comment";
+        comEle.innerHTML = comment;
+        document.getElementById('user-comments-section').prepend(comEle);
+        commentBox.value = "";
+    }
+}
